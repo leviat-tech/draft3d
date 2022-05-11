587de3f74699700f82b5f6c29101b8fbefa0f40a
Calum Hainsworth <calum.hainsworth@leviat.com>
2022-05-11 10:12:06 +0100

    feat: add cylinder entity

--- src/entities/index.js
+++ src/entities/index.js
@@ -1,10 +1,12 @@
 import box from './box';
 import alignedDim from './alignedDim';
 import polygon3d from './polygon3d';
+import cylinder from './cylinder'
 
 
 export {
   box,
   alignedDim,
   polygon3d,
+  cylinder,
 };

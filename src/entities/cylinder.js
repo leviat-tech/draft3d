716ce6a364a7a6200ad1f00c36702307bb78781b
Calum Hainsworth <calum.hainsworth@leviat.com>
2022-05-11 12:02:16 +0100

    fix: typo

--- src/entities/cylinder.js
+++ src/entities/cylinder.js
@@ -14,27 +14,27 @@ const radialAccuracy = 24;
 export default {
   name: 'cylinder',
   parameters: {
-    raduisTop: { name: 'Raduis Top', type: 'number', default: 2 },
-    raduisBottom: { name: 'Raduis Bottom', type: 'number', default: 2 },
+    radiusTop: { name: 'radius Top', type: 'number', default: 2 },
+    radiusBottom: { name: 'radius Bottom', type: 'number', default: 2 },
     length: { name: 'Length', type: 'number', default: 5 },
     color: { name: 'Colour', type: 'color', default: '#6666cc' },
     opacity: { name: 'Opacity', type: 'number', precision: 0.05, default: 1 },
   },
   render(params) {
-    const { raduisTop, raduisBottom, length, color, opacity } = params;
+    const { radiusTop, radiusBottom, length, color, opacity } = params;
 
     const material = createMaterial(color, opacity);
 
-    const geometry = new CylinderGeometry(raduisTop, raduisBottom, length, radialAccuracy);
+    const geometry = new CylinderGeometry(radiusTop, radiusBottom, length, radialAccuracy);
 
     return new Mesh(geometry, material);
   },
   update(object3d, newParams) {
-    const { raduisTop, raduisBottom, length, color, opacity } = newParams;
+    const { radiusTop, radiusBottom, length, color, opacity } = newParams;
 
     updateMaterial(object3d, color, opacity);
 
     object3d.geometry?.dispose();
-    object3d.geometry = new CylinderGeometry(raduisTop, raduisBottom, length, radialAccuracy);
+    object3d.geometry = new CylinderGeometry(radiusTop, radiusBottom, length, radialAccuracy);
   },
 };

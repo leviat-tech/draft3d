df368ebed1892f52f596b58e49c32a34ae7ac549
CalumH0 <104360297+CalumH0@users.noreply.github.com>
2022-05-11 12:06:39 +0100

    feat: add cylinder entity

---
+++ src/entities/cylinder.js
@@ -0,0 +1,40 @@
+import {
+  CylinderGeometry,
+  Mesh,
+} from 'three';
+
+import {
+  createMaterial,
+  updateMaterial,
+} from '../utils/material';
+
+
+const radialAccuracy = 24;
+
+export default {
+  name: 'cylinder',
+  parameters: {
+    radiusTop: { name: 'radius Top', type: 'number', default: 2 },
+    radiusBottom: { name: 'radius Bottom', type: 'number', default: 2 },
+    length: { name: 'Length', type: 'number', default: 5 },
+    color: { name: 'Colour', type: 'color', default: '#6666cc' },
+    opacity: { name: 'Opacity', type: 'number', precision: 0.05, default: 1 },
+  },
+  render(params) {
+    const { radiusTop, radiusBottom, length, color, opacity } = params;
+
+    const material = createMaterial(color, opacity);
+
+    const geometry = new CylinderGeometry(radiusTop, radiusBottom, length, radialAccuracy);
+
+    return new Mesh(geometry, material);
+  },
+  update(object3d, newParams) {
+    const { radiusTop, radiusBottom, length, color, opacity } = newParams;
+
+    updateMaterial(object3d, color, opacity);
+
+    object3d.geometry?.dispose();
+    object3d.geometry = new CylinderGeometry(radiusTop, radiusBottom, length, radialAccuracy);
+  },
+};

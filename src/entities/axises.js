ac5aeb601e7dfd58a016bd126f058d3cb7bf3e15
kwasilonek <you@example.com>
2023-01-03 14:58:42 +0100

    chore: Add force arrows

--- src/entities/axises.js
+++ src/entities/axises.js
@@ -4,6 +4,8 @@ import {
   ArrowHelper,
 } from 'three';
 
+import cylindricalArrow from './cylindricalArrow';
+
 import { createText } from '../utils/geometry';
 
 
@@ -25,23 +27,73 @@ export default {
   name: 'axises',
   parameters: {
     xAxisLength: { name: 'xAxisLength', default: 2, precision: 0.1 },
-    yAxisLength: { name: 'yAxisLength', default: 1, precision: 0.1 },
+    yAxisLength: { name: 'yAxisLength', default: 2, precision: 0.1 },
     zAxisLength: { name: 'zAxisLength', default: 3, precision: 0.1 },
+    xLoads: {
+      name: 'xLoads',
+      default: {
+        force: {
+          value: 100,
+        },
+        moment: {
+          value: 0,
+        },
+      },
+    },
+    yLoads: {
+      name: 'yLoads',
+      default: {
+        force: {
+          value: 100,
+        },
+        moment: {
+          value: 0,
+        },
+      },
+    },
+    zLoads: {
+      name: 'zLoads',
+      default: {
+        force: {
+          value: 100,
+        },
+        moment: {
+          value: 0,
+        },
+      },
+    },
   },
   render(params) {
-    const { xAxisLength, yAxisLength, zAxisLength } = params;
+    const { PI } = Math;
+    const {
+      xAxisLength, yAxisLength, zAxisLength, xLoads, yLoads, zLoads,
+    } = params;
 
     const root = new Object3D();
 
     const xAxis = createArrow(new Vector3(-1, 0, 0), xAxisLength, COLORS.GREEN);
     const xAxisLabel = createText('X', COLORS.GREEN, TEXT_SIZE);
-    xAxisLabel.setRotationFromAxisAngle(new Vector3(0, 1, 0), Math.PI / -2);
+    xAxisLabel.setRotationFromAxisAngle(new Vector3(0, 1, 0), PI / -2);
 
     const yAxis = createArrow(new Vector3(0, 1, 0), yAxisLength, COLORS.BLUE);
     const yAxisLabel = createText('Y', COLORS.BLUE, TEXT_SIZE);
 
     const zAxis = createArrow(new Vector3(0, 0, -1), zAxisLength, COLORS.RED);
     const zAxisLabel = createText('Z', COLORS.RED, TEXT_SIZE);
+
+    // FORCES
+    const xCylindricalArrow = cylindricalArrow.render({ length: 0.2, color: 'green' });
+    const xCylindricalArrowLabel = createText(`Vx = ${xLoads.force.value}`, 'black', 0.07);
+    xCylindricalArrowLabel.rotateX(PI / -2);
+
+    const yCylindricalArrow = cylindricalArrow.render({ length: 0.2, color: 'blue' });
+    const yCylindricalArrowLabel = createText(`Vy = ${xLoads.force.value}`, 'black', 0.07);
+    yCylindricalArrowLabel.rotateZ(PI / 2);
+
+
+    const zCylindricalArrow = cylindricalArrow.render({ length: 0.2, color: 'red' });
+    const zCylindricalArrowLabel = createText(`Vz = ${zLoads.force.value}`, 'black', 0.07);
+    zCylindricalArrowLabel.rotateX(PI / -2);
 
     requestAnimationFrame(() => {
       xAxisLabel.position.x = -calculeLength(xAxisLength);
@@ -52,7 +104,32 @@ export default {
 
       zAxisLabel.position.z = -calculeLength(zAxisLength);
       zAxisLabel.position.x -= zAxisLabel.geometry.boundingSphere.center.x;
+
+      // FORCES
+      xCylindricalArrow.position.x = xAxisLength * -0.7;
+      xCylindricalArrowLabel.position.x = xAxisLength * -0.8;
+      xCylindricalArrowLabel.position.z -= 0.1;
+
+
+      yCylindricalArrow.position.y = yAxisLength * 0.7;
+      yCylindricalArrowLabel.position.y = yAxisLength * 0.6;
+      yCylindricalArrowLabel.position.x -= 0.1;
+
+
+      zCylindricalArrow.position.z = zAxisLength * -0.7;
+      zCylindricalArrowLabel.position.z = zAxisLength * -0.7;
+      zCylindricalArrowLabel.position.x += 0.1;
+
+
+      xCylindricalArrow.rotateZ(xLoads.force.value < 0 ? PI / -2 : PI / 2);
+
+      if (yLoads.force.value < 0) {
+        yCylindricalArrow.rotateX(PI / -2);
+      }
+
+      zCylindricalArrow.rotateX(zLoads.force.value < 0 ? PI / 2 : PI / -2);
     });
+
 
     root.add(xAxis);
     root.add(yAxis);
@@ -61,6 +138,10 @@ export default {
     root.add(xAxisLabel);
     root.add(yAxisLabel);
     root.add(zAxisLabel);
+
+    root.add(new Object3D().add(xCylindricalArrow).add(xCylindricalArrowLabel));
+    root.add(new Object3D().add(yCylindricalArrow).add(yCylindricalArrowLabel));
+    root.add(new Object3D().add(zCylindricalArrow).add(zCylindricalArrowLabel));
 
     return root;
   },

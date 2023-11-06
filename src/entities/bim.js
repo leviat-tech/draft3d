77dbbd328df92e4ec3af6189d4f3aa5d63100f59
kwasilonek <87523973+kwasilonek@users.noreply.github.com>
2023-11-06 12:48:13 +0100

    fix: add arc to bim

--- src/entities/bim.js
+++ src/entities/bim.js
@@ -1,12 +1,127 @@
 import { pick } from 'lodash-es';
 
-import { LatheGeometry, Mesh, Object3D } from 'three';
+import { LatheGeometry, Mesh, Object3D, Path } from 'three';
 
 import { configureInteractivity } from '../utils/helpers';
 import { createMaterial } from '../utils/material';
 import { defineEntity } from '../defineEntity';
 import { createExtrudeGeometry, createPolyCurve } from '../utils/geometry';
 
+
+function getArcX(item1, item2, item3, item4) {
+  const pt1 = (item3.x * item4.z - item4.x * item3.z) * (item2.x - item1.x);
+
+  const pt2 = (item2.x * item1.z - item1.x * item2.z) * (item3.x - item4.x);
+
+  const pt3 = (item2.z - item1.z) * (item3.x - item4.x) - (item3.z - item4.z) * (item2.x - item1.x);
+
+  return (pt1 - pt2) / pt3;
+}
+
+function getArcY(item1, item2, item3, item4, arcX) {
+  if (item2.x !== item1.x) {
+    const pt1 = arcX * (item2.z - item1.z) + item2.x * item1.z - item1.x * item2.z;
+
+    const pt2 = item2.x - item1.x;
+
+    return pt1 / pt2;
+  }
+
+  if (item3.x !== item4.x) {
+    const pt1 = arcX * (item3.z - item4.z) + item3.x * item4.z - item4.x * item3.z;
+
+    const pt2 = item3.x - item4.x;
+
+    return pt1 / pt2;
+  }
+}
+
+function profileDataSlice(params) {
+  let arcPointIndex = 0;
+
+  const paramsCopy = JSON.parse(JSON.stringify(params));
+
+  for (let i = 0; i < paramsCopy.length; i++) {
+    if (paramsCopy[i].type === 'arc_end_point') {
+      if (i >= 2) {
+        arcPointIndex = i - 2;
+        break;
+      }
+    }
+  }
+
+  const return01 = paramsCopy.slice(arcPointIndex, paramsCopy.length);
+  const return02 = paramsCopy.slice(1, arcPointIndex);
+  const result = return01.concat(return02);
+
+  return result.concat(return01[0]);
+}
+
+function profileDataSplice(params) {
+  const paramsCopy = JSON.parse(JSON.stringify(params));
+  const deleteIndices = [];
+
+  paramsCopy.forEach((item, index) => {
+    if (item.type === 'arc_end_point') {
+      if (paramsCopy[index + 1].x === item.x
+        && paramsCopy[index + 1].y === item.y
+        && paramsCopy[index + 1].z === item.z
+      ) {
+        deleteIndices.push(index + 1);
+      }
+    }
+  });
+
+  for (let i = deleteIndices.length - 1; i >= 0; i--) {
+    paramsCopy.splice(deleteIndices[i], 1);
+  }
+  return paramsCopy;
+}
+
+function sortProfileData(params) {
+  const profileDataSliced = profileDataSlice(params);
+  const profileDataSpliced = profileDataSplice(profileDataSliced);
+
+  return profileDataSpliced;
+}
+
+function findExtrusionPath(profile) {
+  const path = new Path();
+  const sortedProfile = sortProfileData(profile);
+
+  sortedProfile.forEach((point, index) => {
+    if (index === 0) {
+      path.moveTo(point.x, point.z);
+      return;
+    }
+
+    if (point.type === 'polyline_point') {
+      path.lineTo(point.x, point.z);
+      return;
+    }
+
+    if (point.type === 'arc_end_point') {
+      const arcX = getArcX(
+        sortedProfile[index - 2],
+        sortedProfile[index - 1],
+        sortedProfile[index],
+        sortedProfile[index + 1],
+      );
+
+      const arcY = getArcY(
+        sortedProfile[index - 2],
+        sortedProfile[index - 1],
+        sortedProfile[index],
+        sortedProfile[index + 1],
+        arcX,
+      );
+
+      path.quadraticCurveTo(arcX, arcY, point.x, point.z);
+    }
+  });
+
+  return path.getPoints();
+}
 
 const defaultBim = {
   insertion_points: {
@@ -38,78 +153,128 @@ const defaultBim = {
     channel: {
       type: 'extrusion',
       profile: [
-        { x: 0.019, y: 0.15, z: -0.004, type: 'polyline_point' },
-        { x: 0.015, y: 0.15, z: 0.0, type: 'arc_end_point' },
-        { x: 0.015, y: 0.15, z: 0.0, type: 'polyline_point' },
-        { x: 0.009, y: 0.15, z: 0.0, type: 'polyline_point' },
-        { x: 0.009, y: 0.15, z: -0.003, type: 'polyline_point' },
-        { x: 0.015, y: 0.15, z: -0.003, type: 'polyline_point' },
-        { x: 0.016, y: 0.15, z: -0.004, type: 'arc_end_point' },
-        { x: 0.016, y: 0.15, z: -0.0135, type: 'polyline_point' },
-        { x: 0.015, y: 0.15, z: -0.0145, type: 'arc_end_point' },
-        { x: -0.015, y: 0.15, z: -0.0145, type: 'polyline_point' },
-        { x: -0.016, y: 0.15, z: -0.0135, type: 'arc_end_point' },
-        { x: -0.016, y: 0.15, z: -0.004, type: 'polyline_point' },
-        { x: -0.015, y: 0.15, z: -0.003, type: 'arc_end_point' },
-        { x: -0.015, y: 0.15, z: -0.003, type: 'polyline_point' },
-        { x: -0.009, y: 0.15, z: -0.003, type: 'polyline_point' },
-        { x: -0.009, y: 0.15, z: 0.0, type: 'polyline_point' },
-        { x: -0.015, y: 0.15, z: 0.0, type: 'polyline_point' },
-        { x: -0.019, y: 0.15, z: -0.004, type: 'arc_end_point' },
-        { x: -0.019, y: 0.15, z: -0.0135, type: 'polyline_point' },
-        { x: -0.015, y: 0.15, z: -0.0175, type: 'arc_end_point' },
-        { x: 0.015, y: 0.15, z: -0.0175, type: 'polyline_point' },
-        { x: 0.019, y: 0.15, z: -0.0135, type: 'arc_end_point' },
-        { x: 0.019, y: 0.15, z: -0.0135, type: 'polyline_point' },
-        { x: 0.019, y: 0.15, z: -0.004, type: 'polyline_point' },
+        { x: 0.0245, y: 0.15, z: -0.03, type: 'polyline_point' },
+        { x: 0.0245, y: 0.15, z: 0.0, type: 'polyline_point' },
+        { x: 0.01048, y: 0.15, z: 0.0, type: 'polyline_point' },
+        {
+          x: 0.01048,
+          y: 0.15,
+          z: -0.00652,
+          type: 'polyline_point',
+        },
+        { x: 0.01207, y: 0.15, z: -0.00774, type: 'arc_end_point' },
+        { x: 0.01959, y: 0.15, z: -0.0034, type: 'polyline_point' },
+        { x: 0.02137, y: 0.15, z: -0.00432, type: 'arc_end_point' },
+        {
+          x: 0.02137,
+          y: 0.15,
+          z: -0.02557,
+          type: 'polyline_point',
+        },
+        { x: 0.02007, y: 0.15, z: -0.02687, type: 'arc_end_point' },
+        {
+          x: -0.02007,
+          y: 0.15,
+          z: -0.02687,
+          type: 'polyline_point',
+        },
+        {
+          x: -0.02137,
+          y: 0.15,
+          z: -0.02557,
+          type: 'arc_end_point',
+        },
+        {
+          x: -0.02137,
+          y: 0.15,
+          z: -0.00432,
+          type: 'polyline_point',
+        },
+        { x: -0.01959, y: 0.15, z: -0.0034, type: 'arc_end_point' },
+        {
+          x: -0.01207,
+          y: 0.15,
+          z: -0.00774,
+          type: 'polyline_point',
+        },
+        {
+          x: -0.01048,
+          y: 0.15,
+          z: -0.00652,
+          type: 'arc_end_point',
+        },
+        {
+          x: -0.01048,
+          y: 0.15,
+          z: -0.00652,
+          type: 'polyline_point',
+        },
+        { x: -0.01048, y: 0.15, z: 0.0, type: 'polyline_point' },
+        { x: -0.0245, y: 0.15, z: 0.0, type: 'polyline_point' },
+        { x: -0.0245, y: 0.15, z: -0.03, type: 'polyline_point' },
+        { x: 0.0245, y: 0.15, z: -0.03, type: 'polyline_point' },
       ],
       path: [
-        { x: 0.00000, y: 0.00000, z: 0.00000, type: 'polyline_point' },
-        { x: 0.00000, y: 0.30000, z: 0.00000, type: 'polyline_point' },
+        { x: 0.0, y: 0.0, z: 0.0, type: 'polyline_point' },
+        { x: 0.0, y: 0.3, z: 0.0, type: 'polyline_point' },
       ],
       length: 0.3,
       cutout: {},
       origin: {
-        x: 0, y: 0, z: 0, alpha: 0, beta: 0, gamma: 0,
+        x: 0,
+        y: 0,
+        z: 0,
+        alpha: 0,
+        beta: 0,
+        gamma: 0,
       },
     },
     anchors: {
       type: 'revolution',
       profile: [
-        { x: 0.0, y: 0.0, z: -0.0809, type: 'polyline_point' },
-        { x: 0.00984, y: 0.0, z: -0.0809, type: 'polyline_point' },
-        { x: 0.00984, y: 0.0, z: -0.079, type: 'polyline_point' },
-        { x: 0.008, y: 0.0, z: -0.079, type: 'polyline_point' },
-        { x: 0.004, y: 0.0, z: -0.07687, type: 'polyline_point' },
-        { x: 0.004, y: 0.0, z: -0.0245, type: 'polyline_point' },
-        { x: 0.00538, y: 0.0, z: -0.0215, type: 'polyline_point' },
-        { x: 0.013, y: 0.0, z: -0.0175, type: 'polyline_point' },
-        { x: 0.0, y: 0.0, z: -0.0175, type: 'polyline_point' },
-        { x: 0.0, y: 0.0, z: -0.0809, type: 'polyline_point' },
+        { x: 0.01235, y: 0.0, z: -0.03, type: 'polyline_point' },
+        { x: 0.005, y: 0.0, z: -0.0342, type: 'polyline_point' },
+        { x: 0.005, y: 0.0, z: -0.107, type: 'polyline_point' },
+        { x: 0.01, y: 0.0, z: -0.1098, type: 'polyline_point' },
+        { x: 0.0109, y: 0.0, z: -0.1098, type: 'polyline_point' },
+        { x: 0.0109, y: 0.0, z: -0.112, type: 'polyline_point' },
+        { x: 0.0, y: 0.0, z: -0.112, type: 'polyline_point' },
+        { x: 0.0, y: 0.0, z: -0.03, type: 'polyline_point' },
+        { x: 0.01235, y: 0.0, z: -0.03, type: 'polyline_point' },
       ],
-      axis_origin: { x: 0.00000, y: 0.00000, z: 0.00000 },
+      axis_origin: { x: 0.0, y: 0.0, z: 0.0 },
       axis_direction: [0, 0, 1],
       sweep_angle: 6.283185307,
       cutout: {},
       origin: [
         {
-          x: 0, y: 0.025, z: 0, alpha: 0, beta: 0, gamma: 0,
+          x: 0, y: 0.035, z: 0, alpha: 0, beta: 0, gamma: 0,
         },
         {
-          x: 0, y: 0.150, z: 0, alpha: 0, beta: 0, gamma: 0,
-        },
-        {
-          x: 0, y: 0.275, z: 0, alpha: 0, beta: 0, gamma: 0,
+          x: 0, y: 0.265, z: 0, alpha: 0, beta: 0, gamma: 0,
         },
       ],
     },
     channel_bounding_box: {
       type: 'cuboid',
-      min_point: { x: -0.019, y: 0.00000, z: -0.01800 },
-      max_point: { x: 0.019, y: 0.30000, z: 0.00000 },
+      min_point: { x: -0.0245, y: 0.0, z: -0.03 },
+      max_point: { x: 0.0245, y: 0.3, z: 0.0 },
       origin: [
         {
           x: 0, y: 0, z: 0, alpha: 0, beta: 0, gamma: 0,
+        },
+      ],
+    },
+    anchors_bounding_box: {
+      type: 'cuboid',
+      min_point: { x: -0.01235, y: -0.01235, z: -0.03 },
+      max_point: { x: 0.01235, y: 0.01235, z: -0.0112 },
+      origin: [
+        {
+          x: 0, y: 0.035, z: 0, alpha: 0, beta: 0, gamma: 0,
+        },
+        {
+          x: 0, y: 0.265, z: 0, alpha: 0, beta: 0, gamma: 0,
         },
       ],
     },
@@ -129,6 +294,33 @@ const defaultBim = {
         },
       ],
     },
+  },
+  log: {
+    high: [
+      'channel',
+      'anchors',
+    ],
+    medium: [
+      'channel_bounding_box',
+      'anchors_bounding_box',
+    ],
+    low: [
+      'channel_bounding_box',
+      'anchors_bounding_box',
+    ],
+  },
+  styles: {
+    common: ['bounding_box', 'channel_bounding_box', 'anchors_bounding_box'],
+    steel: ['channel', 'anchors'],
+  },
+  classification: {
+    uniclass2015_code: 'Pr_20_85_84_84',
+    uniclass2015_description: 'Stainless steel cast-in channels',
+    omniclass_code: '23-13 23 11 17',
+    omniclass_description: 'Mechanical Fasteners for Metal Structures',
+    ifc_export_as: 'IfcMechanicalFastener',
+    ifc_export_type: '',
+    ifc_description: '',
   },
   log: {
     high: [
@@ -177,7 +369,7 @@ function getProfilePath(profile, yOffset = 0) {
 function generateGeometry(type, params) {
   switch (type) {
     case geometryTypes.extrusion: {
-      const path = getProfilePath(params.profile);
+      const path = findExtrusionPath(params.profile);
       const shape = createPolyCurve(path);
       return createExtrudeGeometry(shape, params.length);
     }

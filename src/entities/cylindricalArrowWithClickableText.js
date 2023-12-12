fcac660b39a90d6cc0a596f10746eafd7c1d61bc
Milasinovic, Nikola <milasinovn@ae.com>
2023-12-12 12:55:44 +0100

    feat: extend configuration for rounded cylindrical arrow

--- src/entities/cylindricalArrowWithClickableText.js
+++ src/entities/cylindricalArrowWithClickableText.js
@@ -25,6 +25,13 @@ export default defineEntity({
     isInteractive: { name: 'isInteractive', default: true },
     radius: { name: 'radius', default: 0.02, precision: 0.02 },
     coneHeight: { name: 'coneHeight', default: 0.1, precision: 0.1 },
+    circleRadius: { name: 'circleRadius', default: 0.01, precision: 0.01 },
+    circledArrowConeHeight: { name: 'circledArrowConeHeight', default: 0.05, precision: 0.01 },
+    coneRadius: { name: 'coneRadius', default: 0.03, precision: 0.01 },
+    ellipseCurveRadius: { name: 'ellipseCurveRadius', default: 0.1, precision: 0.01 },
+    rotateZAngle: { name: 'rotateZAngle', default: 174, precision: 1 },
+    positionX: { name: 'positionX', default: 0.015, precision: 0.01 },
+    positionZ: { name: 'positionZ', default: 0.096, precision: 0.01 },
   },
   render(params) {
     const {
@@ -35,12 +42,29 @@ export default defineEntity({
       isRounded,
       radius,
       coneHeight,
+      circleRadius,
+      circledArrowConeHeight,
+      coneRadius,
+      ellipseCurveRadius,
+      rotateZAngle,
+      positionX,
+      positionZ,
     } = params;
 
     const text = createLabel(params.text, params.textSize);
     const textBox = createTextBox(onClick);
     const arrow = isRounded
-      ? roundedCylindricalArrow.config.render({ color, layer })
+      ? roundedCylindricalArrow.config.render({
+        color,
+        circleRadius,
+        circledArrowConeHeight,
+        coneRadius,
+        ellipseCurveRadius,
+        rotateZAngle,
+        positionX,
+        positionZ,
+        layer,
+      })
       : cylindricalArrow.config.render({ length, color, radius, coneHeight, layer });
 
     configureInteractivity(textBox, params);
--- src/entities/roundedCylindricalArrow.js
+++ src/entities/roundedCylindricalArrow.js
@@ -10,16 +10,34 @@ import { defineEntity } from '../defineEntity';
 
 const { PI } = Math;
 
+const radialSegments = 32;
+
 export default defineEntity({
   name: 'roundedCylindricalArrow',
   parameters: {
     color: { name: 'color', default: '#6666aa' },
+    circleRadius: { name: 'circleRadius', default: 0.01, precision: 0.01 },
+    circledArrowConeHeight: { name: 'circledArrowConeHeight', default: 0.05, precision: 0.01 },
+    coneRadius: { name: 'coneRadius', default: 0.03, precision: 0.01 },
+    ellipseCurveRadius: { name: 'ellipseCurveRadius', default: 0.1, precision: 0.01 },
+    rotateZAngle: { name: 'rotateZAngle', default: 174, precision: 1 },
+    positionX: { name: 'positionX', default: 0.015, precision: 0.01 },
+    positionZ: { name: 'positionZ', default: 0.096, precision: 0.01 },
   },
   render(params) {
-    const { color } = params;
+    const {
+      color,
+      circleRadius,
+      circledArrowConeHeight,
+      coneRadius,
+      ellipseCurveRadius,
+      rotateZAngle,
+      positionX,
+      positionZ,
+    } = params;
     const curve = new EllipseCurve(
       0, 0, // ax, aY
-      0.1, 0.1, // xRadius, yRadius
+      ellipseCurveRadius, ellipseCurveRadius, // xRadius, yRadius
       0, PI - (PI / 10), // aStartAngle, aEndAngle
       false, // aClockwise
       0, // aRotation
@@ -38,12 +56,12 @@ export default defineEntity({
       steps: 100,
       extrudePath: curve2,
     };
-    const geometry = new ExtrudeGeometry(createCircle(0.02), extrudeSettings);
+    const geometry = new ExtrudeGeometry(createCircle(circleRadius), extrudeSettings);
 
-    const cone = new Mesh(new ConeGeometry(0.03, 0.05, 32), material);
-    cone.rotateZ(174 * (PI / 180));
-    cone.position.y += 0.015;
-    cone.position.x -= 0.096;
+    const cone = new Mesh(new ConeGeometry(coneRadius, circledArrowConeHeight, radialSegments), material);
+    cone.rotateZ(rotateZAngle * (PI / 180));
+    cone.position.y += positionX;
+    cone.position.x -= positionZ;
 
     const mesh = new Mesh(geometry, material);
 

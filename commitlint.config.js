3ec55bac431aba0bdb457fcc5d74a8edd58e73a6
reeves999999 <55240533+reeves999999@users.noreply.github.com>
2024-03-26 15:32:54 +0000

    chore: revised COMMITLINT

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules:{
-        'subject-case':['lower-case','upper-case']
+        'subject-case':[2,'always',['lower-case','upper-case']]
     }
 
 }

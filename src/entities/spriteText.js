5a90875fc830a0ebfa9c9292a7682194bcc1a49d
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2024-12-02 13:59:55 +0100

    fix: add sprite text element

---
+++ src/entities/spriteText.js
@@ -0,0 +1,36 @@
+import { Object3D } from 'three';
+
+import { createSpriteText } from '../utils/geometry';
+import { configureInteractivity } from '../utils/helpers';
+import { defineEntity } from '../defineEntity';
+
+export function getTextValue({ text, prefix, suffix, formatter }) {
+  const lengthAsString = formatter(text) || text;
+
+  return [prefix, lengthAsString, suffix].filter((val) => val).join(' ');
+}
+
+export default defineEntity({
+  name: 'spriteText',
+  parameters: {
+    prefix: { name: 'Prefix', default: '' },
+    suffix: { name: 'Suffix', default: '' },
+    text: { name: 'Text', default: 'Text' },
+    onClick: { name: 'onClick', default: () => {} },
+    formatter: { name: 'Formatter', default: () => {} },
+    color: { name: 'Colour', type: 'color', default: '#000000' },
+    textSize: { name: 'Text Size', precision: 0.01, default: 0.1 },
+  },
+  render(params) {
+    const root = new Object3D();
+
+    const textValue = getTextValue(params);
+
+    const textObject = createSpriteText(textValue, params.textSize, params.color);
+
+    root.add(textObject);
+    configureInteractivity(textObject, params);
+
+    return root;
+  },
+});

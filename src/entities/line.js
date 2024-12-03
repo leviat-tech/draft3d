61289a7de080047c26566db8b886cdb16abeb21e
Kamil Wasilonek <kamilwasilonek@griddynamics.com>
2024-12-03 15:27:58 +0100

    fix: draw line from points

--- src/entities/line.js
+++ src/entities/line.js
@@ -1,46 +1,39 @@
 import { Vector3, BufferGeometry, Line, LineBasicMaterial } from 'three';
 import { defineEntity } from '../defineEntity';
 
-
 export default defineEntity({
   name: 'line',
   parameters: {
     length: { name: 'length', type: 'number', default: 1 },
     color: { name: 'Colour', type: 'color', default: 'red' },
     opacity: { name: 'Opacity', type: 'number', precision: 0.05, default: 1 },
+    startPoint: { name: 'startPoint' },
+    endPoint: { name: 'startPoint' },
   },
   render(params) {
-    const { length, color, opacity } = params;
+    const { length, color, opacity, startPoint, endPoint } = params;
 
     const material = new LineBasicMaterial({
-      color, opacity, transparent: true,
+      color,
+      opacity,
+      transparent: true,
     });
 
-    const points = [
-      new Vector3(0, 0, 0),
-      new Vector3(length, 0, 0),
-    ];
+    let points;
+
+    if (Array.isArray(startPoint) && Array.isArray(endPoint)) {
+      points = [
+        new Vector3(startPoint[0], startPoint[1], startPoint[2]),
+        new Vector3(endPoint[0], endPoint[1], endPoint[2]),
+      ];
+    } else {
+      points = [new Vector3(0, 0, 0), new Vector3(length, 0, 0)];
+    }
 
     const geometry = new BufferGeometry().setFromPoints(points);
 
     const line = new Line(geometry, material);
 
     return line;
-  },
-  update(object3d, params) {
-    const { length, color, opacity } = params;
-
-    const material = new LineBasicMaterial({
-      color, opacity, transparent: true,
-    });
-
-    const points = [
-      new Vector3(0, 0, 0),
-      new Vector3(length, 0, 0),
-    ];
-
-    object3d.geometry?.dispose();
-    object3d.geometry = new BufferGeometry().setFromPoints(points);
-    object3d.material = material;
   },
 });
